caf4851c1a99b0c7a6322ca34d40a96c6b092a3d
JeonByongHoon <92158935+JeonByongHoon@users.noreply.github.com>
2022-06-21 10:55:27 +0900

    [faker] Currently, the faker python script has a bug to generate datafile.txt. [solution] Add encoding options in script to fix bug. [result] It's possible to generate random sized record datafile.

--- faker/gen.py
+++ faker/gen.py
@@ -1,3 +1,8 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
 from faker import Faker
 from faker.generator import random
 from faker_vehicle import VehicleProvider
