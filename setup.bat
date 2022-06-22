02c6fcc123fc74c58e98ddf74572c63e8f62cf83
JeonByongHoon <92158935+JeonByongHoon@users.noreply.github.com>
2022-06-22 12:48:08 +0900

    [setup.bat] Update batch extend files

--- setup.bat
+++ setup.bat
@@ -10,3 +10,13 @@ XCOPY /y faker\datafile.txt x64\Release\datafile.txt*
 XCOPY /y faker\datafile.txt x64\Debug\datafile.txt*
 xcopy /S /E /H /Y /I "rundata" "x64\Release"
 xcopy /S /E /H /Y /I "rundata" "x64\Debug"
+XCOPY /y mqtt\libcrypto-1_1-x64.dll x64\Release\libcrypto-1_1-x64.dll*
+XCOPY /y mqtt\libcrypto-1_1-x64.dll x64\Debug\libcrypto-1_1-x64.dll*
+XCOPY /y mqtt\libssl-1_1-x64.dll x64\Release\libssl-1_1-x64.dll*
+XCOPY /y mqtt\libssl-1_1-x64.dll x64\Debug\libssl-1_1-x64.dll*
+XCOPY /y mqtt\mosquitto.dll x64\Release\mosquitto.dll*
+XCOPY /y mqtt\mosquitto.dll x64\Debug\mosquitto.dll*
+XCOPY /y mqtt\mosquittopp.dll x64\Release\mosquittopp.dll*
+XCOPY /y mqtt\mosquittopp.dll x64\Debug\mosquittopp.dll*
+XCOPY /y sqlite3\sqlite3.dll x64\Release\sqlite3.dll*
+XCOPY /y sqlite3\sqlite3.dll x64\Debug\sqlite3.dll*
